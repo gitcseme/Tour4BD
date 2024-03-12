68e36e99e3aeea06cb2c40f350694939a20e8d26
kashuvo <shuvo_CSE23@yahoo.com>
2024-03-12 16:27:07 +0600

    refactor: program.cs & remove magic string

--- API/Program.cs
+++ API/Program.cs
@@ -6,8 +6,7 @@ using Membership;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-string tenantDbConnectionString = builder.Configuration.GetConnectionString("TenantDbConnection")!;
-builder.Services.AddPersistence(tenantDbConnectionString);
+builder.Services.AddPersistence(builder.Configuration);
 builder.Services.AddApplication();
 builder.Services.AddMembership();
 
